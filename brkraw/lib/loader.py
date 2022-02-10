1e4d7a9b5bf4b2972818b4a77c298f8a3519189a
dvm-shlee <shlee@unc.edu>
2022-02-10 15:26:22 -0500

    [loader] hotfix 'get_bdata' bug patch

--- brkraw/lib/loader.py
+++ brkraw/lib/loader.py
@@ -825,7 +825,7 @@ class BrukerLoader():
         num_dir = int((num_exp - num_b0) / len(bval))
         bdir = get_value(method, 'PVM_DwDir')
         bvals = np.r_[np.zeros(num_b0), np.concatenate([bval for i in np.ones(num_dir).astype(int)], axis=0)]
-        bvecs = np.concatenate([np.zeros([num_b0, 3])] + [bdir] * len(bval), axis=0).T
+        bvecs = np.concatenate([np.zeros([num_b0, 3])] + [bdir] * len(bvals), axis=0).T
         # test if results are correctly built
         try:
             assert(bvals.shape[0] == num_exp)
