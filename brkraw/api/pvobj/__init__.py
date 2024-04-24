d53f44c16759e60e50c3912959e2d68bd166119b
dvm-shlee <shlee@unc.edu>
2024-04-24 13:16:57 -0400

    [update] minor update for future deidentification support

--- brkraw/api/pvobj/__init__.py
+++ brkraw/api/pvobj/__init__.py
@@ -2,6 +2,6 @@ from .pvdataset import PvDataset
 from .pvscan import PvScan
 from .pvreco import PvReco
 from .pvfiles import PvFiles
-from .parameters import Parameter
+from .parameters import Parameter, Parser
 
-__all__ = [PvDataset, PvScan, PvReco, PvFiles, Parameter]
+__all__ = [PvDataset, PvScan, PvReco, PvFiles, Parameter, Parser]
