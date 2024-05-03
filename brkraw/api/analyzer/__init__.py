9bf0a0ef7e89ee2cc4723d7e2b9431cbfc6ee135
dvm-shlee <shlee@unc.edu>
2024-05-02 21:50:12 -0400

    docs(analyzer): Update and expand docstrings for clarity
    - Revise and enhance docstrings throughout the module to improve code documentation and readability.
    - Add detailed descriptions for class methods and properties, focusing on parameters and behaviors.

--- brkraw/api/analyzer/__init__.py
+++ brkraw/api/analyzer/__init__.py
@@ -1,3 +1,16 @@
+"""Analyzer module initialization.
+
+This module imports and exposes various analyzer classes used to parse and process
+information from raw datasets into more readable formats. Each analyzer provides
+specific functionalities tailored to different aspects of data processing and analysis.
+
+Exposed Classes:
+    BaseAnalyzer: Provides common features and utilities shared among all analyzers.
+    ScanInfoAnalyzer: Specializes in parsing and analyzing scan information from raw datasets.
+    AffineAnalyzer: Handles the computation and analysis of affine matrices from dataset parameters.
+    DataArrayAnalyzer: Focuses on parsing and returning structured data arrays and related metadata.
+"""
+
 from .base import BaseAnalyzer
 from .scaninfo import ScanInfoAnalyzer
 from .affine import AffineAnalyzer
