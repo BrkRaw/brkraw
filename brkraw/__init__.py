8db658ed2a92db17c15defc0085f5f9979b36553
SungHo Lee <shlee@unc.edu>
2024-05-09 10:05:20 -0400

    feat(test) add pytest placefolder and logging feature

--- brkraw/__init__.py
+++ brkraw/__init__.py
@@ -1,13 +1,14 @@
 from .lib import *
 from xnippet import XnippetManager
+from xnippet import setup_logging
 
 __version__ = '0.4.0'
 config = XnippetManager(package_name=__package__, 
-                       package_version=__version__,
-                       package__file__=__file__,
-                       config_filename='config.yaml')
+                        package_version=__version__,
+                        package__file__=__file__,
+                        config_filename='config.yaml')
 
-__all__ = ['BrukerLoader', '__version__', 'config']
+__all__ = ['BrukerLoader', '__version__', 'config', 'setup_logging']
 
 def load(path):
     return BrukerLoader(path)
