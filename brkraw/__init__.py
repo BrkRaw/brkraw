9459963a33ca94e8fe528865def2d3a8898bb8a3
dvm-shlee <shlee@unc.edu>
2024-05-14 16:54:47 -0400

    cleaning(0.3.11)

--- brkraw/__init__.py
+++ brkraw/__init__.py
@@ -1,14 +1,8 @@
 from .lib import *
-from xnippet import XnippetManager
-from xnippet import setup_logging
 
-__version__ = '0.4.0'
-config = XnippetManager(package_name=__package__, 
-                        package_version=__version__,
-                        package__file__=__file__,
-                        config_filename='config.yaml')
+__version__ = '0.3.11'
 
-__all__ = ['BrukerLoader', '__version__', 'config', 'setup_logging']
+__all__ = ['BrukerLoader', '__version__']
 
 def load(path):
     return BrukerLoader(path)
