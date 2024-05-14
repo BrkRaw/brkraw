ec37c4271af3fd2b58bad9e6150b18e73de5722a
dvm-shlee <shlee@unc.edu>
2024-05-14 16:47:40 -0400

    clean(0.3.11)

--- tests/04_api_data_test.py
+++
@@ -1,8 +0,0 @@
-import logging
-from brkraw.api.data import Study
-
-def test_data_init(dataset):
-    for i, pvobj in dataset.items():
-        # if i == 0:
-        studyobj = Study(pvobj.path)
-        logging.info(studyobj.info['header']['date'])
