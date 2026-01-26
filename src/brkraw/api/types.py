d38b088ee67c589181b021a5568d4dbb6017bdf6
SungHo Lee <shlee@unc.edu>
2026-01-26 18:04:29 -0500

    feat(api): introduce public API layer

---
+++ src/brkraw/api/types.py
@@ -0,0 +1,39 @@
+from ..apps.loader.types import (
+    StudyLoader, 
+    ScanLoader, 
+    RecoLoader
+)
+from ..apps.loader.types import (
+    GetAffineType,
+    GetDataobjType,
+    ConvertType,
+    ConverterHook,
+    SubjectType, 
+    SubjectPose, 
+    AffineSpace,
+    Affines,
+    Dataobjs,
+    Metadata,
+    ConvertedObj,
+    HookArgs
+)
+from ..core.parameters import Parameters
+
+__all__ = [
+    "GetAffineType",
+    "GetDataobjType",
+    "ConvertType",
+    "ConverterHook",
+    "StudyLoader",
+    "ScanLoader",
+    "RecoLoader",
+    "SubjectType",
+    "SubjectPose",
+    "AffineSpace",
+    "Affines",
+    "Dataobjs",
+    "Metadata",
+    "ConvertedObj",
+    "HookArgs",
+    "Parameters"
+]
